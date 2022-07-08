557a07a93c32dc5afc67c4cd8ec225428b8c744e
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:01:42 +0530

    Add project files.

---
+++ JumpStatement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpStatement
+{
+    internal class Program
+    {
+        //jump statement
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+            //if(i==2)
+            //{
+            //    continue;
+            //}
+
+            abc:
+                Console.WriteLine("I am label");
+                if (i == 2)
+                {
+                    goto abc;
+                  
+                }
+                if (i == 3)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
